fadf9118c6610d7c04e43c35b2722b09e5efedb6
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-27 19:55:51 -0500

    ⛓️ cell.py

---
+++ ARIA/CELL/cell.py
@@ -0,0 +1,33 @@
+# cell.py
+# Cell 
+# Where the player resides most of their time, interacting with objects, engaging in personal activities, and receiving messages from ARIA or other characters. 
+# Time spent: High
+
+# Cell Inventory & Function Index:
+
+# Bed: The standard issue bed found in each prisoner's cell. It serves as a place to sleep or rest during downtime.
+
+# Toilet: A basic toilet facility located next to the bed. It allows prisoners to relieve themselves without leaving their cells.
+
+# Sink: An overhead sink above the toilet area. Used for washing hands, faces, or other personal hygiene tasks. Water pressure may be limited.
+
+# Lockers: Two small lockers located near the entrance of the cell. One locker contains personal belongings (clothing, shoes, etc.), while the other houses sanitary supplies (toothbrush, toothpaste, soap, shampoo). These lockers can be secured using numeric codes known only to the prisoner and staff members.
+
+# Exercise Equipment: A minimalist exercise set consisting of a pull-up bar and stationary bike. This equipment helps prisoners maintain some level of fitness within their confined spaces.
+
+# Reading Materials: A small selection of books and magazines provided by the prison library. These materials vary in content and quality, catering to different interests and reading levels.
+
+# Writing Supplies: A pen and notepad placed on the bedside table. Prisoners use these to communicate with others, write letters, or engage in creative activities like journaling or writing stories.
+
+# Light Switch: Located on the wall near the door, the light switch controls the artificial lighting within the cell. There might be a dimmer function for adjusting light intensity during sleep or relaxation periods.
+
+# Door: Made of solid steel reinforced with security features such as cameras, microphones, and sensors. The door can only be opened by authorized personnel using special keys or remote access devices. Attempting to break down the door would result in immediate alarm activation and response from security personnel.
+
+# Security Camera: A small camera mounted on the ceiling directly above the bed. Its primary purpose is to monitor prisoner activity within the cell for safety reasons; however, it may also record any unauthorized actions taken by inmates.
+
+# Microphone: Similar to the camera, a microphone is hidden within the cell ceiling. It picks up sounds inside the cell and transmits them to monitoring stations where guards can listen for potential issues or rule violations.
+
+# Emergency Button: Located on the wall near the door, this button triggers an alarm signal when pressed. It is intended for use in cases of emergencies or serious threats to the prisoner's wellbeing. Abuse of this feature may result in disciplinary action.
+
+The cell environment provides both functional necessities and opportunities for mental stimulation and personal growth. While life within its confines is restrictive, the items and resources available offer prisoners ways to cope, adapt, and even thrive under these harsh conditions.
+
