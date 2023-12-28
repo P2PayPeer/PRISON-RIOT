1cd8f15c9cd86e29b4ca4edc476a23b00bf5e09f
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-27 20:03:17 -0500

    ⛓️ showers.py

---
+++ ARIA/SHOWERS/showers.py
@@ -0,0 +1,26 @@
+# showers.py
+# Showers 
+# A communal area where inmates bathe and potentially engage in illicit activities due to lack of direct supervision. 
+# Time spent: Moderate
+
+# Shower Room Inventory & Function Index:
+
+# Individual Showers: A row of enclosed shower stalls lining one wall. Each stall contains a showerhead, drain, and small shelf for placing belongings temporarily. Privacy screens made of plastic or metal separate the stalls but do not fully conceal the occupants.
+
+# Sinks: Several sink units located along another wall. These contain mirrors above, soap dispensers, and hand dryers. Inmates use these to wash their hands before and after showering.
+
+# Lockers: Small lockers placed beside each shower stall. These store personal items such as clothing, shoes, towels, and any other possessions inmates wish to protect while bathing. Like cell lockers, they require numeric codes known only to the prisoner and staff members for access.
+
+# Drainage System: A series of drains installed in the floor beneath the shower stalls to collect water runoff during bathing. They are designed to minimize standing water and reduce potential slipping hazards.
+
+# Exhaust Fan: A large fan mounted high on the ceiling above the shower stalls. Its primary function is to remove steam and moisture from the air, improving ventilation within the room. It also helps mask any sounds occurring inside the shower stalls from eavesdroppers outside the room.
+
+# Camera Coverage: Similar to the prison yard, the shower room features surveillance cameras hidden within the ceiling. These record activities within the room for safety reasons and can be accessed by guards monitoring the footage in real-time.
+
+# Emergency Button: Located near the entrance of the shower room, this button triggers an alarm signal when pressed. It is intended for use in cases of emergencies or serious threats to an inmate's wellbeing. Abuse of this feature may result in disciplinary action.
+
+# Supervisor Station: An elevated platform positioned in a corner of the shower room. This area houses controls for adjusting temperature, water pressure, and other shower settings. Additionally, it provides a vantage point for supervisors to observe inmate behavior without intruding upon their privacy.
+
+# In the confined setting of the shower room, inmates must navigate complex dynamics involving privacy, trust, and vulnerability. 
+# While the lack of direct supervision offers opportunities for discreet interactions and potential acts of rebellion, it also increases risks associated with exposure or exploitation.
+# The shower room serves as a microcosm reflecting many aspects of life within PRISON-RIOT – a place where power struggles continue unabated, even amidst the most intimate of activities.
