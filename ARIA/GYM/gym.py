4ab6ea6cbe130d9804ccff2362f14b37b91c00f4
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-27 20:27:59 -0500

    ⛓️ gym.py

---
+++ ARIA/GYM/gym.py
@@ -0,0 +1,29 @@
+# gym.py
+# Exercise Room 
+# A gym-like area where inmates can maintain physical fitness, which can impact their abilities and overall well-being in the game. 
+# Time spent: Very Low
+
+# Exercise Room Inventory & Function Index:
+
+# Weightlifting Area: Equipped with barbells, dumbbells, and weight plates of varying sizes, allowing inmates to engage in resistance training exercises.
+
+# Cardiovascular Equipment: Treadmills, stationary bikes, elliptical machines, and rowing machines providing options for aerobic exercise and endurance building.
+
+# Free Weights: A selection of kettlebells, medicine balls, and plyometric boxes available for unstructured strength and conditioning training.
+
+# Stretching Mats: Thick mats placed around the perimeter of the exercise room for warming up and cooling down during workouts.
+
+# Mirrors: Positioned along walls opposite the exercise equipment, mirrors allow inmates to observe their form and technique during workouts.
+
+# Lockers: Small lockers adjacent to the exercise area for storing personal belongings and items such as extra clothing and shoes worn during workout sessions.
+
+# Cameras: Hidden cameras monitoring activity within the exercise room, capturing footage for security purposes and potentially revealing useful information about individual inmates' habits or capabilities.
+
+# Water Fountain: A single water fountain located near the entrance of the exercise room, providing access to hydration during workouts.
+
+# Security Staff: Guards assigned to the exercise room keep watch over inmates participating in physical activities, ensuring safety and adherence to rules.
+
+# The exercise room serves as a vital component of player health and wellbeing within Raiyaku Penitentiary. 
+# Engaging in regular physical activity can improve stamina, strength, and agility, which in turn affect performance in various aspects of the game. 
+# While the time spent here is minimal compared to other locations, it remains essential for long-term success and survival within PRISON-RIOT. 
+# Players should take advantage of this opportunity not only to maintain physical fitness but also to observe and interact with fellow inmates in a more casual setting.
