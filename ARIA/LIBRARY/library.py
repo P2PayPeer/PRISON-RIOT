41f9098767fdef98d472e5ca26a11ed00b48594b
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-27 20:16:47 -0500

    ⛓️ library.py

---
+++ ARIA/LIBRARY/library.py
@@ -0,0 +1,26 @@
+# library.py
+# Library / Study Area 
+# A secluded spot where players can access resources, learn new skills, or gather intelligence about other characters and prison operations. 
+# Time spent: Low-Medium
+
+
+# Library/Study Area Inventory & Function Index:
+
+# Bookshelves: Well-organized rows of shelves housing various books, magazines, and publications covering diverse topics such as history, science, literature, philosophy, and more.
+
+# Computers: Several computer terminals connected to a secure network offering access to online research databases, email services, and limited social media platforms. These computers are closely monitored by prison authorities to ensure no unauthorized activities take place.
+
+# Reference Materials: A collection of encyclopedias, dictionaries, thesauri, and other reference works available for in-library use only.
+
+# Comfortable Seating: Plush chairs and couches scattered throughout the library, providing a cozy atmosphere conducive to reading, studying, or engaging in quiet conversations.
+
+# Quiet Zone: A designated area within the library where silence is strictly enforced to create an optimal environment for focused study and research.
+
+# Printer/Copier: A shared multifunction device allowing users to print, copy, or scan documents and images. Usage is restricted and monitored by staff members.
+
+# Security Cameras: Hidden cameras mounted on walls and ceilings, capturing footage of all activities within the library. Their primary purpose is to maintain safety and order but may also be used for monitoring individual behavior or identifying potential threats to the institution.
+
+# Emergency Button: Located near the entrance of the library, this button triggers an alarm signal when pressed. It is intended for use in cases of emergencies or serious threats to the wellbeing of any individuals within the library. Abuse of this feature may result in disciplinary action.
+
+# Inside the library/study area, players have ample opportunities to expand their knowledge base, hone existing skills, and gather valuable intel about the characters and inner workings of PRISON-RIOT. The serene atmosphere encourages introspection and critical thinking, making it an ideal setting for plotting and scheming. 
+# However, players must also remain vigilant against potential threats posed by both fellow inmates and the ever-watchful eye of prison authority figures.
