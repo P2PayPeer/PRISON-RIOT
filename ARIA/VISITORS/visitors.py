922a36d2532290c85522ff7f691f39c6424b7ef5
Mind-Interfaces <94399418+Mind-Interfaces@users.noreply.github.com>
2023-12-27 20:06:11 -0500

    ⛓️ visitors.py

---
+++ ARIA/VISITORS/visitors.py
@@ -0,0 +1,27 @@
+# visitors.py
+# Visitation Room
+# A controlled environment where inmates meet with loved ones or associates under the watchful eye of guards. This is an important location for information exchange and emotional support. 
+# Time spent: Moderate
+
+# Visitation Room Inventory & Function Index:
+
+# Tables: Rectangular tables arranged in pairs facing each other, separated by a thin strip of fabric serving as a barrier between participants. These tables provide seating for inmates and their visitors during their scheduled visits.
+
+# Chairs: Matching chairs placed alongside the tables for visitor comfort. They are typically made of durable materials designed to withstand extensive use and regular cleaning.
+
+# Glass Barriers: Clear partitions separating each table pair. These barriers allow for visual contact between inmates and their visitors while maintaining a sense of separation and security.
+
+# Guard Stations: Elevated platforms located at strategic points throughout the visitation room. Guards stationed here monitor all activity via surveillance cameras and two-way communication systems. Their presence deters violent incidents or unauthorized exchanges.
+
+# Vending Machines: Similar to those found in the prison yard, these machines offer snacks, drinks, and basic essentials like toiletries for purchase using a prepaid system accessible only to inmates.
+
+# Waiting Area: A section of the room designated for visitors awaiting their turn to see the inmate. It includes benches, magazines, and televisions for entertainment or distraction.
+
+# Announcement System: A loudspeaker system capable of broadcasting messages and instructions related to visitation procedures, scheduling changes, or facility rules.
+
+# Recording Devices: Cameras and audio equipment situated throughout the visitation room capture every interaction between inmates and their visitors. These recordings serve as evidence should disputes arise or suspected rule violations occur.
+
+# Sanitization Station: An area equipped with soap, water, and disposable towels for visitors to clean their hands before and after contact with the inmate.
+
+#The visitation room plays a crucial role within Raiyaku Penitentiary, providing an opportunity for inmates to maintain connections with the outside world and receive essential emotional support. However, due to its controlled nature and constant surveillance, it also presents unique challenges and dangers. 
+#Inmates must navigate the fine line between honest conversation and clandestine planning, while guards work diligently to prevent unauthorized exchanges that could threaten the stability of PRISON-RIOT.
